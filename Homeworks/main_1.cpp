586ce71757763656953d660b5981cd1a2462588e
Samvel Karapetyan <87465611+karapetyan2@users.noreply.github.com>
2022-03-03 12:09:06 +0400

    Create main_1.cpp

---
+++ Homeworks/main_1.cpp
@@ -0,0 +1,13 @@
+/*Write an application which takes input from the user and calculates the sum up to the entered natural number*/
+
+#include <iostream>
+
+int main() {
+
+	int a={1};
+	int b={2};
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	return 0;
+}
