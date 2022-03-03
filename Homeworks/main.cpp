b9909195bbdb1e4d56304ca0842d06da93391479
Samvel Karapetyan <87465611+karapetyan2@users.noreply.github.com>
2022-03-03 11:50:10 +0400

    Create main.cpp

---
+++ Homeworks/main.cpp
@@ -0,0 +1,6 @@
+/*Write an application to swap two numbers entered by user using temporary variable.
+Write an application calculating the product of two floating-point numbers entered by the user and printing it on the screen:
+Write an application to find the largest number among the three numbers entered by the user.
+Write an application to check whether a character entered by the user is an alphabet or not.
+Write an application which takes input from the user and calculates the sum up to the entered natural number:*/
+
