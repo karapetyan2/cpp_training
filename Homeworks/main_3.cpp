0990cd891c2b7eb1b095f6283cdb146c9600cce9
Samvel Karapetyan <87465611+karapetyan2@users.noreply.github.com>
2022-03-03 14:41:17 +0400

    Create main_3.cpp

---
+++ Homeworks/main_3.cpp
@@ -0,0 +1,33 @@
+/*Write an application to find the largest number among the three numbers entered by the user.*/
+#include <iostream>
+
+int main() {
+	int a{};
+	int b{};
+	int c{};
+	std::cout << "Pleas input first number: ";
+	std::cin >>  a;
+	std::cout << "Pleas input second number: ";
+	std::cin >>  b;
+	std::cout << "Pleas input third number: ";
+	std::cin >>  c;
+	
+	std::cout << "" << std::endl;
+	if(a > b && a > c) {
+		std::cout << "Largest number: " << a << std::endl;	
+	}else if(b > a && b > c) {
+		std::cout << "Largest number: " << b << std::endl;
+	}else if(c > a && c > b) {
+		std::cout << "Largest number: " << c << std::endl;
+	}
+	
+	std::cout << "" << std::endl;
+	if(a = b) {
+		std::cout << "Equal numbers: " << a << " = " << b << std::endl;
+	}else if(b = c) {
+		std::cout << "Equal numbers: " << b << " = " << c << std::endl;
+	}else if(a = c) {
+		std::cout << "Equal numbers: " << a << " = " << c << std::endl;
+	}
+	return 0;
+}
